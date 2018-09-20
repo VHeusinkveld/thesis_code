5ca8f5a43434173a466d208d7fac8f6728f7788d
VHeusinkveld <vincent.heusinkveld@gmail.com>
2018-09-20 22:18:23 +0200

    Work in progress on edges of rotor.

--- experiments/2D_rotor/rotor.c
+++ experiments/2D_rotor/rotor.c
@@ -9,8 +9,9 @@ int minlevel, maxlevel; // Min and max grid level 2^n
 double err;
 
 // Rotor
-double rP;			// Power
-double rD, rA;			// Diameter, Area
+double rTdamp;		// Time to start up rotor
+double rP;		// Power
+double rD, rA;		// Diameter, Area
 double rx0, ry0;	// Origin	
 
 // Data analysis 
@@ -23,15 +24,16 @@ int main() {
 	err= 0.005;
 	
 	// Grid initialization 
-	init_grid(1<<7);
+	init_grid(1<<9);
 	L0 = 10.;
 	origin (0, 0);
 
 	// Rotor details
-	rP = 10;
-	rD = 0.1 + 0.0001*sq(2);  
-	rx0 = 5;
-	ry0 = 8 + 0.0001*sq(2);
+	rTdamp = 3.;
+	rP = 50.;
+	rD = 0.5 + 0.0001*sq(2);  
+	rx0 = 5.;
+	ry0 = 8. + 0.0001*sq(2);
 	rA = rD;
 
 	run();
@@ -62,33 +64,30 @@ event rotor (i=1; i++) {
 			// Takes care of rotor edges 
 			double d_start = fabs(x - (rx0 - rD/2.));
 			double d_end = fabs(x - (rx0 + rD/2.));
-			double c, c1, c2;
+			double c=0, c1=0, c2=0;
 			
+			// TODO Check if this goes correct 
 			if (d_start < Delta/2.) {
 				if ((x - (rx0 - rD/2)) < 0){
 					c1 = (Delta/2. - d_start)/Delta;
 				} else {
 					c1 = (Delta/2. + d_start)/Delta;
 				}
-			} else {
-				c1 = 0.5;
 			}
-
+			
 			if (d_end < Delta/2.){
 				if (((rx0 + rD/2) - x) < 0){
 					c2 = (Delta/2. - d_end)/Delta;
 				} else {
 					c2 = (Delta/2. + d_end)/Delta;
 				}
-			} else {
-				c2 = 0.5;
 			}
 				
-			c = c1 + c2; // Includes the option of both ends in one cell 
-			
-			// Calculate actual addition to the kinetic energy 
-			double temp = pow(u.y[], 3.) - 2*c*rP*dt/rA*Delta/Delta;
-
+			c = 1; //(c1 + c2 > 0.01) ? c1 + c2 : 1.; // Includes the option of both ends in one cell 
+			// Calculate actual addition to the kinetic energy
+			double damp = t < rTdamp ? t/rTdamp : 1.; // Linear rotor startup
+			double temp = pow(u.y[], 3.) - damp*c*2.*rP*dt/rA*Delta/Delta;
+			//printf("i=%d, c1=%g, c2=%g, c=%g \n",i , c1, c2, c);
 			if (temp < 0.) {
 				u.y[] = -pow(abs(temp), 1./3.);
 			} else {

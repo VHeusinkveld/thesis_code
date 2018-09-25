ba52e677367778110ab51450ef452ec2ba54b4d8
VHeusinkveld <vincent.heusinkveld@gmail.com>
2018-09-25 15:23:29 +0200

    Latest line rotor, stable release

--- experiments/2D_rotor/rotor.c
+++ experiments/2D_rotor/rotor.c
@@ -9,10 +9,7 @@ int minlevel, maxlevel; // Min and max grid level 2^n
 double err;
 
 // Rotor
-double rTdamp;		// Time to start up rotor
-double rP;		// Power
-double rD, rA;		// Diameter, Area
-double rx0, ry0;	// Origin
+struct Rotor Rot;
 
 // Data analysis 
 vertex scalar omega[]; 	// Vorticity
@@ -21,27 +18,36 @@ double e_old;
 
 // FUNCTIONS
 // =================================================================================
- 
+struct Rotor {	
+	double Tdamp;			// Time to start up rotor
+	double P;			// Power
+	double D, L, A, V;		// Diameter, Thickness, Area ,Volume
+	double x0, y0;			// Origin
+	double theta, phi;		// Polar and Azimuthal angle 
+};
 
 // Check if gridcell is in fan domain 
-bool near_rotor(double x, double y, double Delta, double rx0, double ry0, double rD) {
+bool near_rotor(double x, double y, double Delta, struct Rotor Rot) {
 	bool x_dom, y_dom;
 
-	x_dom = (x + Delta/2. >= rx0 - rD/2.) && (x - Delta/2. <= rx0 + rD/2.);
-	y_dom = (y - Delta/2. <= ry0) && (y + Delta/2. >= ry0);
+	double xt = 1.*x;
+	double yt = 1.*y;
+
+	x_dom = (xt + Delta/2. >= Rot.x0 - Rot.D/2.) && (xt - Delta/2. <= Rot.x0 + Rot.D/2.);
+	y_dom = (yt + Delta/2. >= Rot.y0 - Rot.L/2.) && (yt - Delta/2. <= Rot.y0 + Rot.L/2.);
+		
 
 	return x_dom && y_dom; 
 }
 
-
-
+	
 // Code
 // =================================================================================
 int main() {
 	// Adaptivity 
 	minlevel = 3;
-	maxlevel = 8;
-	err= 0.005;
+	maxlevel = 7;
+	err= 0.01;
 	
 	// Grid initialization 
 	init_grid(1<<6);
@@ -49,12 +55,20 @@ int main() {
 	origin (0, 0);
 
 	// Rotor details
-	rTdamp = 5.;
-	rD = 1. + 0.00001*sqrt(2);  
-	rx0 = 5.;
-	ry0 = 5. + 0.00001*sqrt(2);
-	rA = rD;
-	rP = rA*0.5;
+	Rot.Tdamp = 5.;
+
+	Rot.D = 1. + 0.00001*sqrt(2);
+	Rot.L = 0.;
+	Rot.A = Rot.D;
+	Rot.V = Rot.A;
+
+	Rot.x0 = 5.;
+	Rot.y0 = 5. + 0.00001*sqrt(2);
+	
+	Rot.theta = 0.;
+	Rot.phi = 0.;
+	
+	Rot.P = Rot.V*0.5;
 	
 	DT = 0.1;
 
@@ -77,31 +91,31 @@ event init(t = 0) {
 	mu = muc;
 }
 
-event diag(t=0; t+=0.1) {
+event diag(t=0; t+=0.5) {
 	double e = 0.;
-	double rE = rP*(t < rTdamp ? t/rTdamp : (t-(rTdamp*0.5)));
+	double rE = Rot.P*(t < Rot.Tdamp ? t/Rot.Tdamp : (t-(Rot.Tdamp*0.5)));
 
 	foreach(reduction(+:e)) {
 		e += 0.5*(u.x[]*u.x[] + u.y[]*u.y[])*sq(Delta);
 	}
 	
-	printf("de=%g, drE=%g, de/drE=%g  \n", e-e_old, rP*0.1, (e-e_old)/(rP*0.1));
+	printf("de=%g, drE=%g, de/drE=%g  \n", e-e_old, Rot.P*0.5, (e-e_old)/(Rot.P*0.5));
 	e_old = 1.*e;
 }
 
-event rotor(i=1; i++) {
+event forcing(i=1; i++) {
 		
 	foreach () {
 		// Checks if gridcell is in the rotor
-		if (near_rotor(x, y, Delta, rx0, ry0, rD)) {
+		if (near_rotor(x, y, Delta, Rot)) {
 			
 			// Takes care of rotor edges 
-			double d_start = fabs(x - (rx0 - rD/2.));
-			double d_end = fabs(x - (rx0 + rD/2.));
+			double d_start = fabs(x - (Rot.x0 - Rot.D/2.));
+			double d_end = fabs(x - (Rot.x0 + Rot.D/2.));
 			double c=0, c1=0, c2=0;
 			
 			if (d_start < Delta/2.) {
-				if ((x - (rx0 - rD/2)) < 0){
+				if ((x - (Rot.x0 - Rot.D/2)) < 0){
 					c1 = (Delta/2. - d_start)/Delta;
 				} else {
 					c1 = (Delta/2. + d_start)/Delta;
@@ -109,7 +123,7 @@ event rotor(i=1; i++) {
 			}
 			
 			if (d_end < Delta/2.){
-				if (((rx0 + rD/2) - x) < 0){
+				if (((Rot.x0 + Rot.D/2) - x) < 0){
 					c2 = (Delta/2. - d_end)/Delta;
 				} else {
 					c2 = (Delta/2. + d_end)/Delta;
@@ -120,9 +134,9 @@ event rotor(i=1; i++) {
 			c = (c1 + c2 > 0) ? ((c1 > 0 && c2 > 0) ? c1 + c2 - Delta : c1 + c2) : 1.; 
 
 			// Calculate actual addition to the kinetic energy
-			double damp = (t < rTdamp) ? t/rTdamp : 1.; // Linear rotor startup
+			double damp = (t < Rot.Tdamp) ? t/Rot.Tdamp : 1.; // Linear rotor startup
 			
-			double temp = pow(u.y[], 3.) - damp*c*2.*rP/rA;
+			double temp = pow(u.y[], 3.) - damp*c*2.*Rot.P/Rot.V;
 				
 			if (temp < 0.) {
 				u.y[] = -pow(fabs(temp), 1./3.);
@@ -130,6 +144,7 @@ event rotor(i=1; i++) {
 				u.y[] = pow(temp, 1./3.);
 			
 			}
+			//u.y[] -= dt*(-1 - u.y[]);
 		}
 	} 
 	// TODO is this needed?	
@@ -139,7 +154,7 @@ event rotor(i=1; i++) {
 
 event adapt(i++) {
 	adapt_wavelet((scalar *){u},(double []){err,err},maxlevel,minlevel);
-	refine(near_rotor(x, y, 4*Delta, rx0, ry0, rD) && level < maxlevel);
+	refine(near_rotor(x, y, 4*Delta, Rot) && level < maxlevel);
 
 }
 
@@ -158,7 +173,7 @@ event movies(t += 0.1) {
 }
 
 // Final event
-event end(t += 2; t <= 30) {
+event end(t += 2; t <= 10) {
 	printf("i = %d t = %g\n", i, t);
 }
 

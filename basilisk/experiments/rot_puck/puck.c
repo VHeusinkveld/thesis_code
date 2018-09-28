3adb48ea66e87ea8ba052d2bdad231732e85f8a3
VHeusinkveld <vincent.heusinkveld@gmail.com>
2018-09-28 14:12:34 +0200

    Free rotatable puck

--- basilisk/experiments/rot_puck/puck.c
+++ basilisk/experiments/rot_puck/puck.c
@@ -7,28 +7,47 @@
 int main()
 {
   init_grid(128);
-  double L0 = 10.
-  origin(0, 0, 0);
+  double L0 = 1.;
+  X0 = Y0 = Z0 = -L0/2.;
   
-  double R = 0.5;
-  double w = 0.1;
+  double R = 0.25;
+  double w = 0.10;
   
-  double xf = L0/2.;
-  double yf = L0/2.;
-  double zf = L0/2.;
+  double xf = 0.;
+  double yf = 0.;
+  double zf = 0.;
   scalar fan[], sph[], plane[];
   fan.prolongation = fraction_refine;
-  view(theta = 0.1, phi = 0.3); 
+  view(theta = 0.1, phi = 0.3);
 
-  for (double rTheta = 0.,  double rPhi = 0.; rTheta < M_PI; rTheta += M_PI/12, rPhi += M_PI/6)
+  double rTheta = 0.;  
+  double rPhi = 0.;
+
+  double steps = 121.;
+  for (double i = 0.; i < steps; i+=1.)
   {
-    double[3] rn = {sin(rTheta)*cos(-rPhi + M_PI/2.), sin(rTheta)*sin(-rPhi + M_PI/2.), np.cos(rTheta)}
+    if(i<=40.){
+      rTheta += 2*M_PI/40.;
+      rPhi = 0.;
+    }
+    if( (i>40.) && (i<=80.)){
+      rTheta = M_PI/2;
+      rPhi += 2*M_PI/40.;
+    }
+    if(i>80.){
+      rTheta += 2*M_PI/40.;
+      rPhi += 2*M_PI/40.;
+    }
 
-    fraction(sph, sq(R) - sq(rn[0]*(x - xf)) sq(rn[1]*(y - yf)) - sq(rn[2]*(z - zf)));
-    fraction(plane, rn[0]*x + rn[1]*y + rn[2]*z - w/2.);
+    printf("i=%g, theta=%g, phi=%g \n", i, rTheta, rPhi);
+	
+    double rn[3] = {sin(rTheta)*cos(-rPhi + M_PI/2.), sin(rTheta)*sin(-rPhi + M_PI/2.), cos(rTheta)};
+
+    fraction(sph, -sq((x - xf)) - sq((y - yf)) - sq((z - zf)) + sq(R));
+    fraction(plane, rn[0]*x + rn[1]*y + rn[2]*z + w/2.);
     foreach ()
       fan[] = sph[] * plane[];
-    fraction(plane, -rn[0]*x + -rn[1]*y + -rn[2]*z - w/2.);
+    fraction(plane, -rn[0]*x - rn[1]*y - rn[2]*z + w/2.);
     foreach ()
       fan[] *= plane[];
     clear();
@@ -36,6 +55,6 @@ int main()
     cells(alpha = 0);
     draw_vof("fan");
     save("puck.mp4");
-    adapt_wavelet({fan}, (double[]){0.01}, 3, 7);
+    adapt_wavelet({fan}, (double[]){0.01}, 7, 7);
   }
 }

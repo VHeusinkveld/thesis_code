8aab895c98af47abca55d35b13595d30bd5ad8b5
VHeusinkveld <vincent.heusinkveld@gmail.com>
2018-09-28 12:13:36 +0200

    Implemented 3D puck in basilisk

--- basilisk/experiments/rot_puck/puck.c
+++ basilisk/experiments/rot_puck/puck.c
@@ -1,47 +1,41 @@
-/**
-# A Puck
-
-After the succes of 
-[Alexis](http://basilisk.fr/sandbox/aberny/csgBool.c) we try to
-construct a shape.
-*/
 #include "grid/octree.h"
 #include "utils.h"
 #include "fractions.h"
 #include "view.h"
 
-/**
-A puck is defined here as the volume between the perpendicular
-intersections of two planes and an infinitely long cylinder.
 
-![Its shape corresponds to that of a fan?](puck/puck.mp4)
- */
-
-int main(){
-  init_grid(64);
-  double R = 0.25;
-  double w = 0.05;
-  X0 = Y0 = Z0 = -L0/2;
-  double xf = 0;
-  double yf = 0;
-  double zf = 0;
-  scalar fan[], cyl[], plane[];
+int main()
+{
+  init_grid(128);
+  double L0 = 10.
+  origin(0, 0, 0);
+  
+  double R = 0.5;
+  double w = 0.1;
+  
+  double xf = L0/2.;
+  double yf = L0/2.;
+  double zf = L0/2.;
+  scalar fan[], sph[], plane[];
   fan.prolongation = fraction_refine;
-  view(theta = 0.1, phi = 0.3);
-  for (double dir = 0; dir < 3*pi; dir += 0.01){
-    fraction(cyl,  sq(R) - sq(cos(dir)*(x - xf) - sin(dir)*(y - yf)) - sq(z - zf));
-    fraction(plane, -cos(dir)*(y - yf - cos(dir)*w/2.) - sin(dir)*(x - xf - sin(dir)*w/2.));
-    foreach()
-      fan[] = cyl[] * plane[];
-    fraction(plane, cos(dir)*(y - yf + cos(dir)*w/2.) + sin(dir)*(x - xf + sin(dir)*w/2.));
-    foreach()
+  view(theta = 0.1, phi = 0.3); 
+
+  for (double rTheta = 0.,  double rPhi = 0.; rTheta < M_PI; rTheta += M_PI/12, rPhi += M_PI/6)
+  {
+    double[3] rn = {sin(rTheta)*cos(-rPhi + M_PI/2.), sin(rTheta)*sin(-rPhi + M_PI/2.), np.cos(rTheta)}
+
+    fraction(sph, sq(R) - sq(rn[0]*(x - xf)) sq(rn[1]*(y - yf)) - sq(rn[2]*(z - zf)));
+    fraction(plane, rn[0]*x + rn[1]*y + rn[2]*z - w/2.);
+    foreach ()
+      fan[] = sph[] * plane[];
+    fraction(plane, -rn[0]*x + -rn[1]*y + -rn[2]*z - w/2.);
+    foreach ()
       fan[] *= plane[];
     clear();
     boundary({fan});
     cells(alpha = 0);
     draw_vof("fan");
     save("puck.mp4");
-    adapt_wavelet({fan}, (double[]){0.01}, 3, 5);
+    adapt_wavelet({fan}, (double[]){0.01}, 3, 7);
   }
 }
-

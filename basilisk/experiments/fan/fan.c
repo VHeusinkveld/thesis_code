d02d26fd25ce41b210966d4ad196d56603df9f30
VHeusinkveld <vincent.heusinkveld@gmail.com>
2018-10-02 13:57:40 +0200

    Combined puck and rotor in fan, arbitrary angle can be selected. Also enhanced some of the code structure.

--- basilisk/experiments/fan/fan.c
+++ basilisk/experiments/fan/fan.c
@@ -11,6 +11,7 @@ Global variables
 
 int minlevel, maxlevel;
 double eps;
+struct sRotor rot; 
 scalar fan[];
 
 /*
@@ -25,7 +26,7 @@ struct sRotor {
 	double R, W, A, V;		// Diameter, Thickness, Area ,Volume
 	double x0, y0, z0;		// Origin of rotor
 	double theta, phi;		// Polar and Azimuthal angle 
-	double nf[3], nr[3];	        // Normal vector fan, rotation 
+	coord nf, nr;			// Normal vector fan, rotation 
 };
 
 /*
@@ -41,26 +42,26 @@ struct sRotor rotor_init() {
 	
     // Set variables 
     rot.rampT = 0.;
-	rot.R = 0.25  + 0.00001*sqrt(2);     
-	rot.W = 0.1   + 0.00001*sqrt(2);                      
+	rot.R = 0.1   + 0.00001*sqrt(2);     
+	rot.W = 0.005 + 0.00001*sqrt(2);                      
     rot.Prho = 1.;
     
    	rot.x0 = rot.y0 = rot.z0 = L0/2.;
 	rot.z0 = 0.;
-	rot.theta = 0.; // Polar angle
+	rot.theta = M_PI/2.; // Polar angle
 	rot.phi = 0.;	// Azimuthal angle 
 
    	// Set normal vectors 
-   	rot.nr[0] = sin(rot.theta)*cos(rot.phi);
-	rot.nr[1] = sin(rot.theta)*sin(rot.phi);
-	rot.nr[2] = cos(rot.theta);
+   	rot.nf.x = sin(rot.theta)*cos(rot.phi);
+	rot.nf.y = sin(rot.theta)*sin(rot.phi);
+	rot.nf.z = cos(rot.theta);
 
-	rot.nf[0] = sin(rot.theta)*cos(-rot.phi + M_PI/2.);
-    rot.nf[1] = sin(rot.theta)*sin(-rot.phi + M_PI/2.);
-    rot.nf[2] = cos(rot.theta);
+	rot.nr.x = sin(rot.theta)*cos(rot.phi);
+    rot.nr.y = sin(rot.theta)*sin(rot.phi);
+    rot.nr.z = cos(rot.theta);
 
     // Calculate consequences
-    rot.A = 1.*rot.R;                      
+    rot.A = sq(rot.R)*M_PI;                      
 	rot.V = rot.A*rot.W;
 	rot.P = rot.V*rot.Prho;
 
@@ -68,17 +69,19 @@ struct sRotor rotor_init() {
 }
 
 /* Function returning the volume fractions of a fan object */
-void rotor_coord(struct sRotor rot) {
-    scalar sph[], plnu[], plnd[];
-    fan.prolongation = fraction_refine; // Tell basilisk it is a volume field
-    fraction(sph, -sq((x - rot.x0)) - sq((y - rot.y0)) - sq((z - rot.z0)) + sq(rot.R));
-    fraction(plnu, rot.nr[0]*(x - rot.x0) + rot.nr[1]*(y - rot.y0) + rot.nr[2]*(z - rot.z0) + rot.W/2.);
-    fraction(plnd, -rot.nr[0]*(x - rot.x0) + -rot.nr[1]*(y - rot.y0) + -rot.nr[2]*(z - rot.z0) + rot.W/2.);
-    
-    foreach () {
-    	fan[] = sph[] * plnu[] * plnd[];
-    }
-        
+void rotor_coord() {
+
+      	scalar sph[], plnu[], plnd[];
+
+   	fan.prolongation = fraction_refine; // Tell basilisk it is a volume field
+    	fraction(sph, -sq((x - rot.x0)) - sq((y - rot.y0)) - sq((z - rot.z0)) + sq(rot.R));
+    	fraction(plnu,  rot.nr.x*(x - rot.x0) + rot.nr.y*(y - rot.y0) + rot.nr.z*(z - rot.z0) + rot.W/2.);
+    	fraction(plnd, -rot.nr.x*(x - rot.x0) - rot.nr.y*(y - rot.y0) - rot.nr.z*(z - rot.z0) + rot.W/2.);	
+
+	foreach () {
+    		fan[] = sph[] * plnu[] * plnd[];
+   	}
+
 }
 
 
@@ -97,15 +100,13 @@ int main() {
    	X0 = Y0 = Z0 = 0.;
 
    	// Initialize physics 
-   	struct sRotor rot = rotor_init(); 
-	//TODO incompatible types when init type double using type struct sRotor
-	rotor_coord(rot);
-  	//mu = {0, 0, 0}
+   	rot = rotor_init(); 
+	//mu = {0, 0, 0}
 
   	// Adaptivity
-  	minlevel = 7; 
+  	minlevel = 5; 
   	maxlevel = 8;
-  	eps = 0.05;
+  	eps = 0.005;
 
   	foreach_dimension() {
    		periodic (right);
@@ -116,13 +117,24 @@ int main() {
 
 /* Forcing by the rotor */
 event forcing(i = 1; i++) {
-	double ugoal = 1.;
+
+	rotor_coord();
+
 	foreach() {
-        if(fan[] > 0.) {
-			printf(" yes");
-            u.y[] = u.y[] + (ugoal - u.y[])*dt;
-        }
-    }
+	if(fan[] > 0.) {
+	foreach_dimension() {
+		double wsgn = sign(rot.nf.x*u.x[]) + (sign(rot.nf.x*u.x[]) == 0)*sign(rot.nf.x);
+		double w = wsgn*sq(rot.nf.x)*(2./rho[])*(rot.P/rot.V)*dt;
+		double utemp = sq(u.x[]) + w;
+
+		double usgn = 1.*(u.x[] >= 0)*(utemp > 0) +
+			     -1.*(u.x[] >= 0)*(utemp < 0) +
+			      1.*(u.x[] <  0)*(utemp < 0) +
+			     -1.*(u.x[] <  0)*(utemp > 0); 
+		u.x[] = usgn*sqrt(fabs(utemp));
+	}	
+	}	
+	}
 }
 
 /* Progress event */
@@ -138,12 +150,15 @@ event adapt(i++) {
 /* Visualisation */
 event movies(t += 0.1) {
     vertex scalar omega[]; 	// Vorticity
-	scalar lev[];	 	// Grid depth 
+	scalar lev[];	 	// Grid depth
+	scalar ekin[];		// Kin energy 
 	foreach () {
 		omega[] = ((u.y[1,0] - u.y[-1,0]) - (u.x[0,1] - u.x[0,-1]))/(2*Delta); // Curl(u) 
 		lev[] = level;
+		ekin[] = rho[]*cube(Delta)*(sq(u.x[]) + sq(u.y[]) + sq(u.z[]));
 	}
 	boundary ({lev});
+	output_ppm (ekin, file = "ppm2mp4 ekin.mp4", n = 512);
 	output_ppm (u.x, file = "ppm2mp4 vel_x.mp4", n = 512, linear = true, min = -1, max = 1);
 	output_ppm (u.y, file = "ppm2mp4 vel_y.mp4", n = 512, linear = true, min = -1, max = 1);
 	output_ppm (omega, file = "ppm2mp4 vort.mp4", n = 512, linear = true); 

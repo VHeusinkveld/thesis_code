971053d3bfba7702e8bbfe69a65693b711f19917
Vheusinkveld <vincent.heusinkveld@gmail.com>
2018-10-16 17:53:52 +0200

    added cells that rotate with puck

--- basilisk/experiments/rot_puck/puck.c
+++ basilisk/experiments/rot_puck/puck.c
@@ -17,7 +17,8 @@ int main() {
   double zf = 1.*L0/2.;
   scalar fan[], sph[], plane[];
   fan.prolongation = fraction_refine;
-  view(theta = -0.1, phi = 0.3, tx=-L0/2., ty=-L0/2.);
+  view(tx=0., ty=-yf);
+  view(theta=M_PI/4., phi=M_PI/6.);
   double rTheta = 0.;  
   double rPhi = 0.;
 
@@ -39,6 +40,7 @@ int main() {
     printf("i=%g, theta=%g, phi=%g \n", i, rTheta, rPhi);
 	
     double rn[3] = {sin(rTheta)*cos(-rPhi + M_PI/2.), sin(rTheta)*sin(-rPhi + M_PI/2.), cos(rTheta)};
+    //double rl[3] = {sin(rTheta+M_PI/2.)*cos(-rPhi + M_PI/2.), sin(rTheta+M_PI/2.)*sin(-rPhi + M_PI/2.), cos(rTheta+M_PI/2.)};
 
     fraction(sph, -sq((x - xf)) - sq((y - yf)) - sq((z - zf)) + sq(R));
     fraction(plane, rn[0]*(x-xf) + rn[1]*(y-yf) + rn[2]*(z-zf) + w/2.);
@@ -49,9 +51,13 @@ int main() {
       fan[] *= plane[];
     clear();
     boundary({fan});
-    double fac = 0.1*L0;
-    double alp = rn[0]*(xf + fac*rn[0]) + rn[1]*(yf + fac*rn[1]) + rn[2]*(zf + fac*rn[2]);
-    cells(n = {rn[0],rn[1],rn[2]}, alpha = alp);
+    //double fac = 0.*L0;
+    //double alp = rn[0]*(xf + fac*rn[0]) + rn[1]*(yf + fac*rn[1]) + rn[2]*(zf + fac*rn[2]);
+    //double alp2 = rl[0]*(xf + fac*rl[0]) + rl[1]*(yf + fac*rl[1]) + rl[2]*(zf + fac*rl[2]);
+    //cells(n = {rn[0],rn[1],rn[2]}, alpha = alp);
+    //cells(n = {rl[0],rl[1],rl[2]}, alpha = alp2);
+    cells(n = {xf,0,0}, alpha=xf/2.);
+    cells(n = {0,0,zf}, alpha=zf/2.);
     draw_vof("fan");
     save("puck.mp4");
     adapt_wavelet({fan}, (double[]){0.01}, 7, 4);
